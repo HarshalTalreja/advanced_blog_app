4b0fada2a3275da5b2f2039c85e591fd86bd3aaa
HarshalTalreja <talrejaharshal50@gmail.com>
2020-05-10 03:52:53 +0530

    removed venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Harshal Talreja\Documents\harshal_blog\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
