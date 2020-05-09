1043ca246c4d3bc96e12069fc8d1aca31d47475a
HarshalTalreja <talrejaharshal50@gmail.com>
2020-05-10 03:52:11 +0530

    Heroku Setings updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn harshal_blog.wsgi
--- harshal_blog/settings.py
+++ harshal_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&a+2o-y5mt%4+vv5b%athr*^kikrlk^n^r87bt+8cs2wxqwy-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hiblogg.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -139,9 +142,9 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-AWS_ACCESS_KEY_ID = 'AKIA5LMUYZJLI2Q22IO3'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'hiblog-bucket'
+AWS_ACCESS_KEY_ID='AKIA5LMUYZJLI2Q22IO3'
+AWS_SECRET_ACCESS_KEY='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'
+AWS_STORAGE_BUCKET_NAME='hiblog-bucket'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
@@ -152,3 +155,5 @@ AWS_S3_REGION_NAME = "ap-south-1"
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_ADDRESSING_STYLE = "virtual"
+
+django_heroku.settings(locals())
